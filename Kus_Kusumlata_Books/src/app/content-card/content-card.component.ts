6070ec087f22693b34b0d4a23626b76a3af0f866
kusuml04 <KK670@myscc.ca>
2023-05-21 23:05:49 -0400

    Create Content-list compnonent and delete content-list.ts

--- Kus_Kusumlata_Books/src/app/content-card/content-card.component.ts
+++ Kus_Kusumlata_Books/src/app/content-card/content-card.component.ts
@@ -1,51 +1,9 @@
 import { Component } from '@angular/core';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss']
 })
-export class ContentCardComponent {
-  myList: ContentList;
-  constructor(){
-
-    this.myList = new ContentList();
- 
-
-    this.myList.addItem({
-      id: 0,
-      title: "The Immortals of Meluha",
-      description: "The novel is set in ancient India and follows the life of Shiva, a warrior.",
-      creator: "Amish Tripathi",
-      imgURL: "assets/img/img1.jpg",
-      type:"Fantasy"
-    }); 
-    this.myList.addItem({
-      id: 1,
-      title: "The White Tiger ",
-      description: "The novel is a darkly funny story about a young man named Balram Halwai who goes from being a poor driver in a tiny hamlet to becoming a successful entrepreneur in Bangalore.",
-      creator: "Aravind Adiga",
-      imgURL: "assets/img/img2.jpg",
-      type: "Fiction"
-    });
-    this.myList.addItem({
-      id: 2,
-      title: "Five Point Someone",
-      description: "The novel follows three friends, Hari, Ryan, and Alok, while they study engineering at the prestigious Indian Institute of Technology (IIT) in Delhi.",
-      creator: "Chetan Bhagat",
-      imgURL: "assets/img/img3.jpg",
-      type: "Humor"
-    });
-
-  
-
-    // myList.items = [];
-  }
-
-
-  ngOnInit(): void {
-
-  }
-
+export class ContentCardComponent{
 }
---
+++ Kus_Kusumlata_Books/src/app/content-list/content-list.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.scss']
+})
+export class ContentListComponent {
+    contentListArray =[
+    {
+      id: 0,
+      title: "The Immortals of Meluha",
+      description: "The novel is set in ancient India and follows the life of Shiva, a warrior.",
+      creator: "Amish Tripathi",
+      imgURL: "assets/img/img1.jpg",
+      type:"Fantasy"
+    },
+    {
+      id: 1,
+      title: "The White Tiger ",
+      description: "The novel is a darkly funny story about a young man named Balram Halwai who goes from being a poor driver in a tiny hamlet to becoming a successful entrepreneur in Bangalore.",
+      creator: "Aravind Adiga",
+      imgURL: "assets/img/img2.jpg",
+      type: "Fiction"
+    },
+    {
+      id: 2,
+      title: "Five Point Someone",
+      description: "The novel follows three friends, Hari, Ryan, and Alok, while they study engineering at the prestigious Indian Institute of Technology (IIT) in Delhi.",
+      creator: "Chetan Bhagat",
+      imgURL: "assets/img/img3.jpg",
+      type: "Humor"
+    },
+    {
+      id: 3,
+      title: 'The Way I Used To Be',
+      description: 'It tells the powerful and emotionally charged story of a teenage girl named Eden, who grapples with the aftermath of a traumatic event that changes her life forever.',
+      creator: 'Amber Smith',
+      imgURL: 'assets/img/img4.jpg',
+      type: 'Young Adult Fiction',
+      
+    },
+    {
+      id: 4,
+      title: 'Beach Read',
+      description: 'The story revolves around January Andrews, a bestselling romance author, who finds herself in a writing slump after her fathers death and a shocking revelation about his secret life.',
+      creator: 'Emily Henry',
+      imgURL: 'assets/img/img5.jpg',
+      type: 'Romance',
+      
+    }
+    
+    ];
+
+    showContent(getContent: any) {
+      console.log('Content ID:', getContent.id);
+      console.log('Content Title:', getContent.title);
+  }
+
+  }
